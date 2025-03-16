97018b4e1f0ab8e108807013cff3ca61a38ec077
Manjula-designer <110459234+Manjula-designer@users.noreply.github.com>
2025-03-16 11:45:25 +0000

    bg-color-changer

--- chaiaurcode/05_background_changer/src/App.jsx
+++ chaiaurcode/05_background_changer/src/App.jsx
@@ -1,15 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [color, setColor] = useState('olive')
+const red=()=>{
+  setColor('red')
+}
+const green=()=>{
+  setColor('green')
+}
+const blue=()=>{
+  setColor('blue')
+}
   return (
-    <>
-      
-    </>
+    <div className='w-full h-screen ' style={{backgroundColor:color}}>
+       <div className='fixed justify-center bottom-12 inset-x-0 px-2 flex flex-wrap'>
+       <div className='flex flex-wrap bg-white justify-center px-2 py-3 rounded-3xl gap-3 shadow-lg '>
+        <button onClick={red} className='outline-none px-3 py-1 rounded-full shadow-lg text-white ' style={{backgroundColor:'red'}} >red</button> 
+        <button onClick={green} className='outline-none px-3 py-1 rounded-full shadow-lg text-white' style={{backgroundColor:'green'}} >green</button>
+        <button onClick={blue} className='outline-none px-3 py-1 rounded-full shadow-lg text-white' style={{backgroundColor:'blue'}} >blue</button>
+        </div>
+        
+       </div>
+       
+    </div>
+   
   )
 }
 
